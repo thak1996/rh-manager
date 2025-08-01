7af14e399ab1678c3bda485c3863e964df74712f
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-08-01 02:16:19 -0300

    Refactor User model and create UserDetail and UserDepartament models; remove unused migration files

--- database/migrations/2025_08_01_050835_create_users_table.php
+++ database/migrations/2025_08_01_050835_create_users_table.php
@@ -13,12 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('department_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('role', 50);
+            $table->string('permissions', 1000);
             $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -28,5 +37,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
